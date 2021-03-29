637314895faa5d17fdf83ca73d17ecff53786371
Нефедов Никита <grand.marchello@gmail.com>
2021-03-29 16:11:17 +0400

    Решение задания 4 (Исправлено)

--- src/main/FuelGauge.java
+++ src/main/FuelGauge.java
@@ -11,7 +11,7 @@ public class FuelGauge {
         this.fuel = fuel;
     }
 
-    int refueling(){
+    public int refueling(){
 
         System.out.println("Заправляем полный бак! (50 Литров)");
         if (fuel < 50){
@@ -19,7 +19,7 @@ public class FuelGauge {
         }
         return fuel;
     }
-    void moving(int fuel, int range){
+    public void moving(int fuel, int range){
         if(fuel > 0) {setFuel (fuel - (range - lastrange)/10);lastrange = range;
             System.out.println("Пройдено: " + range);
         }
--- src/main/Odometer.java
+++ src/main/Odometer.java
@@ -1,12 +1,12 @@
 public class Odometer {
-    int range;
+    public int range,maxrange = 999999;
     FuelGauge fuelgauge = new FuelGauge();
-    void move(int fuel){
+    public void move(int fuel){
         fuel = fuelgauge.getFuel();
-        if(range > 999999){ range = 0;
+        if(range > maxrange){ range = 0;
         }
         else if (fuel > 0){
-            System.out.println("Едем");range+=50;fuelgauge.moving(fuel,range);
+            range+=50;fuelgauge.moving(fuel,range);
         }
         else{fuelgauge.setFuel(fuelgauge.refueling());
         }
